ea7ae4ccce5c9ebee8c93f261e7ee06a1b430728
KittenAmogus <kittenamogus3@gmail.com>
2026-07-18 20:33:35 +0300

    Fix branch in irq.s

--- src/irq.s
+++ src/irq.s
@@ -6,29 +6,29 @@
 .align 2
 .global _irq_handler
 _irq_handler:
-  blx powerdown
+  bl powerdown
 
 .align 2
 .global _nmi_handler
 _nmi_handler:
-  blx powerdown
+  bl powerdown
 
 .align 2
 .global _hardfault_handler
 _hardfault_handler:
-  blx powerdown
+  bl powerdown
 
 .align 2
 .global _svcall_handler
 _svcall_handler:
-  blx powerdown
+  bl powerdown
 
 .align 2
 .global _pendsv_handler
 _pendsv_handler:
-  blx powerdown
+  bl powerdown
 
 .align 2
 .global _systick_handler
 _systick_handler:
-  blx powerdown
+  bl powerdown
